51a75a33b6d00912eef8d13aa1bbab40959a3fcf
Raman Subedi <ramansubedi5321@gmail.com>
2025-10-07 16:19:56 +0530

    Actually fix CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,13 @@ app.use(express.json({ limit: '10mb' }));
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
+    res.status(200).end();
+    return;
   }
   
   next();
